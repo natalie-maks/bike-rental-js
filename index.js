d30ee5fc1cdb732eacef32d020b1e27f4b85ef81
natalie-maks <natasha.maxymenko@gmail.com>
2023-03-26 02:09:47 +0100

    Created responsive navbar

---
+++ index.js
@@ -0,0 +1,18 @@
+const menuBtn = document.getElementById("menu-btn");
+const menuList = document.getElementById("menu-list");
+
+menuBtn.addEventListener("click", () => {
+  menuBtn.classList.toggle("open");
+  if (menuBtn.classList.contains("open")) {
+    menuList.classList.remove("hidden");
+    menuList.classList.add("flex");
+    setTimeout(() => {
+      menuList.classList.add("open");
+    }, 50);
+  } else {
+    menuList.classList.remove("open");
+    setTimeout(() => {
+      menuList.classList.add("hidden");
+    }, 250);
+  }
+});
